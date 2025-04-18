67d030c9d0b3c9dc7103b6d0def8fca4014642b8
Akande Lateef <akandelateef0@gmail.com>
2025-04-18 19:20:02 +0100

    fix

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
+const dotenv = require('dotenv')
+dotenv.config();
 const chatRoutes = require('./routes/chat');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
